728fa5a3a7fb1410e7993dcefced49addebf5caa
Shivam <shivam@sovtechnologies.com>
2024-08-05 11:13:02 +0530

    adding cors policy

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import indexRoute from './src/routes/index.js'
 import errorHandler from './src/error/errorHandler.js'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/api',indexRoute)
